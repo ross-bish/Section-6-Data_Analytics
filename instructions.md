6a04b399f1ef8e7cbf1326e9523641b8027c51af
ross-bish <83789503+ross-bish@users.noreply.github.com>
2023-12-12 21:56:01 +0000

    Add files via upload

---
+++ instructions.md
@@ -0,0 +1,24 @@
+# Accessing Data 📚
+
+In this section we will write Python code to read in, store and retrieve data from files stored in your machine.
+
+
+## Part A 👨‍💻
+Creating, writing to and closing a ``.txt`` file.
+
+
+## Part B 👨‍💻
+Creating, writing to and reading a ``.csv`` file. 
+
+
+## Steps
+1. Follow the instructions laid out in pages 74 - 76 and see if you can understand how to _create, read from / write to_, different file types.
+2. Use the `#commments` provided to help you lay out your code.
+3. Run your code to check that it functions properly.
+4. Notice that the following files should appear beneath `main.py`
+
+![image](image.png) 
+
+
+
+  
---
+++ main.py
@@ -0,0 +1,52 @@
+# ---------------------------------
+# Title: Accessing Data
+# Date: 22/2/23
+# ---------------------------------
+
+
+
+### Part A - Creating a Text file
+
+
+
+# The "w" argument indicates write
+# The "a" argument indicates append (add to end of file)
+# The "r" argument indicates read
+
+
+
+
+
+
+
+
+### Part B - Creating a CSV file
+
+# The "w" argument indicates write
+
+
+
+# The "r" argument indicates read
+# reads the info as a single string
+
+
+
+# separates string into list based on specified delimiter (in this case comma " , ") 
+
+
+
+
+### Note: the items in the list are strings. This is because the file.read() function always reads data as a string. 
+
+### We can use list-comprehension to convert the data in our list into an int or float or suitable data type.
+
+
+
+# Uses [ ] to show it is returning a list
+
+
+
+
+
+
+### Note: the word "item" appears twice. This is a variable name and it needs to be the same in both locations in the line of code.
