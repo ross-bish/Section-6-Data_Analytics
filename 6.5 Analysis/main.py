f1b04022dbeeb006e3cd5aa5afe532ad2ac66613
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 18:27:35 +0000

    Add files via upload

---
+++ 6.5 Analysis/main.py
@@ -0,0 +1,73 @@
+# ----------------------
+# Title: Data Analysis
+# Date:
+# ----------------------
+
+
+
+
+# ---------------------
+# 1. Min and Max values
+
+
+
+
+
+
+# ----------------------------------
+# 2. Calculating the mean - method 1 
+
+
+
+
+
+
+# ----------------------------------
+# 2. Calculating the mean - method 2
+
+
+
+
+
+
+# -------------------------------------------
+# 2. Calculating the mean - Statistics Module
+
+
+
+
+
+# -------------------------------------
+# 3. Finding the median - manual method
+
+
+
+
+
+
+# -----------------------------------------
+# 3. Finding the median - statistics module
+
+
+
+
+
+
+# ------------------------
+# 4. Finding the frequency
+
+
+
+
+
+
+# -----------------------------------
+# 5. Finding the mode - manual method
+
+
+
+
+
+# ---------------------------------------
+# 5. Finding the mode - statistics module
+
