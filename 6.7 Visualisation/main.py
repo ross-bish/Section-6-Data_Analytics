87bed00cf73f08758bb76a07f8798f9ed1bc8afb
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 18:33:16 +0000

    Add files via upload

---
+++ 6.7 Visualisation/main.py
@@ -0,0 +1,14 @@
+# ------------------------
+# Title: Visualisation
+# Date: 7/3/23
+# ------------------------
+
+import matplotlib.pyplot as plt
+
+numBS = [1,2.5,4,3,2.5,2]
+plt.plot(numBS)
+plt.title("Brothers and Sisters")
+plt.xlabel("List Index")
+plt.ylabel("Num of Brothers & Sisters")
+plt.show()
+
